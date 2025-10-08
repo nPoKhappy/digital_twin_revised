296fd6aa4873d9b318f3d5758e57a375159e85e7
Jacky Chou <jj612121@gmail.com>
2025-10-08 13:11:48 +0800

    ann model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,7 @@
 from .gru_model import Seq2Seq as GRUModel
 from .transformer_model import Seq2Seq as TransformerModel
+from .ann_model import Seq2Seq as ANNModel
+from .ann_model import SimpleANNSeq2Seq as SimpleANNModel
 
 def get_model(config):
     """
@@ -30,6 +32,26 @@ def get_model(config):
             embedding_dim=model_params['embedding_dim'],
             hidden_dim=model_params['hidden_dim'],
             n_layers=model_params['n_layers']
+        )
+    elif model_name == 'ann':
+        return ANNModel(
+            num_en_input=num_en_input,
+            num_de_input=num_de_input,
+            num_output=num_output,
+            embedding_dim=model_params['embedding_dim'],
+            hidden_dim=model_params['hidden_dim'],
+            n_layers=model_params['n_layers'],
+            dropout=model_params.get('dropout', 0.1)
+        )
+    elif model_name == 'simple_ann':
+        return SimpleANNModel(
+            num_en_input=num_en_input,
+            num_de_input=num_de_input,
+            num_output=num_output,
+            embedding_dim=model_params['embedding_dim'],
+            hidden_dim=model_params['hidden_dim'],
+            n_layers=model_params['n_layers'],
+            dropout=model_params.get('dropout', 0.1)
         )
     else:
         raise ValueError(f"未知的模型名稱: {model_name}")
