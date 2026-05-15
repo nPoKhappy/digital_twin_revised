12f1b70f2fc8ddc5e30e3f65990e0435d68f5f10
Jacky Chou <jj612121@gmail.com>
2026-05-15 13:35:51 +0800

    原本 ann 的相關配置做刪除

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,7 @@
 from .gru_model import Seq2Seq as GRUModel
 from .gru_s2s import GRUSeq2Seq as GRUExact
 from .transformer_model import Seq2Seq as TransformerModel
-from .ann_model import Seq2Seq as ANNModel
-from .ann_model import SimpleANNSeq2Seq as SimpleANNModel
+
 
 # Import user defined models
 from .user_defined_transformer import TransformerModel as TransformerLayerwise
@@ -45,26 +44,7 @@ def get_model(config):
             n_heads=model_params.get('num_heads', 1),
             dropout=model_params.get('dropout', 0.1)
         )
-    elif model_name == 'ann':
-        return ANNModel(
-            num_en_input=num_en_input,
-            num_de_input=num_de_input,
-            num_output=num_output,
-            embedding_dim=model_params['embedding_dim'],
-            hidden_dim=model_params['hidden_dim'],
-            n_layers=model_params['n_layers'],
-            dropout=model_params.get('dropout', 0.1)
-        )
-    elif model_name == 'simple_ann':
-        return SimpleANNModel(
-            num_en_input=num_en_input,
-            num_de_input=num_de_input,
-            num_output=num_output,
-            embedding_dim=model_params['embedding_dim'],
-            hidden_dim=model_params['hidden_dim'],
-            n_layers=model_params['n_layers'],
-            dropout=model_params.get('dropout', 0.1)
-        )
+    
     # User Defined Models
     elif model_name == 'transformer_layerwise':
         return TransformerLayerwise(
